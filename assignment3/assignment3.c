8ede9ed60bd7bb46f4dafba50ae68c9c6dba69ff
Spencer <spencerthomasr@gmail.com>
2020-02-15 12:26:11 -0800

    init commit

---
+++ assignment3/assignment3.c
@@ -0,0 +1,21 @@
+/***********************************************************************
+name:
+	readable -- recursively count readable files.
+description:	
+	See CS 360 Files and Directories lecture for details.
+***********************************************************************/
+
+/* Includes and definitions */
+
+/**********************************************************************
+Take as input some path. Recursively count the files that are readable.
+If a file is not readable, do not count it. Return the negative value
+of an error codes you may get. However, consider what errors you want
+to respond to.
+**********************************************************************/
+
+
+int readable(char* inputPath) {
+	//This is where you do the work. ;)
+    return 0;
+}
